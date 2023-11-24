a4c723d9e3cc9c67b75bce36dd995cc09c9aeedd
Zac <a19860101@gmail.com>
2023-11-24 14:10:13 +0800

    build

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,9 +1,9 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_c765b6bd.mjs';
+import { manifest } from './manifest_b31e3734.mjs';
 
 const _page0  = () => import('./chunks/generic_6c7ac396.mjs');
-const _page1  = () => import('./chunks/index_1e9625dc.mjs');
+const _page1  = () => import('./chunks/index_19967031.mjs');
 const _page2  = () => import('./chunks/post-1_8bc44985.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/posts/post-1.md", _page2]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
